0be8842d1cf55ca14b9a74b508b964ea6f0bedbb
mayssaGH <mayssagnm@gmail.com>
2026-03-04 14:32:14 +0100

    fix: correct 'previous' button taking to next unit, and move addXP() call (when loading kanji not when clicking 'next' button)

--- src/main/resources/static/scripts/pages/gradeKanjiPage.js
+++ src/main/resources/static/scripts/pages/gradeKanjiPage.js
@@ -11,49 +11,48 @@ let previousBtn = document.getElementById('previous');
 
 let nextUnit;
 let previousUnit;
-let link;
 
 async function displayButtons(data) {
-    console.log("data: ", data);
+    let nextLink;
+    let prevLink;
+
     if (previousUnit.kanji != "null") {
         previousBtn.classList.remove("hidden");
         if (previousUnit.isTest !== 0) {
-            link = `/learn/grades/${data.grade}/tests/${previousUnit.isTest}`;
+            prevLink = `/learn/grades/${data.grade}/tests/${previousUnit.isTest}`;
             previousBtn.innerText = "TEST";
-            nextBtn.classList.add("test");
+            previousBtn.classList.add("test");
         } else {
-            link = `/learn/grades/${data.grade}/${previousUnit.kanji}`;
+            prevLink = `/learn/grades/${data.grade}/kanji?kanji=${previousUnit.kanji}&id=${id - 1}`;
         }
         previousBtn.addEventListener("click", async function () {
             await playAudio("/sounds/next.mp3");
-            window.location.href = link;
+            window.location.href = prevLink;
         });
     }
     if (nextUnit.kanji == "null") {
-        link = `/learn/grades/${data.grade}/tests/final`;
+        nextLink = `/learn/grades/${data.grade}/tests/final`;
         nextBtn.innerText = "FINAL TEST";
         nextBtn.classList.add("test");
     } else {
         if (nextUnit.isTest !== 0) {
-            link = `/learn/grades/${data.grade}/tests/${nextUnit.isTest}`;
+            nextLink = `/learn/grades/${data.grade}/tests/${nextUnit.isTest}`;
             nextBtn.innerText = "TEST";
             nextBtn.classList.add("test");
         } else {
-            link = `/learn/grades/${data.grade}/kanji?kanji=${nextUnit.kanji}&id=${id + 1}`;
+            nextLink = `/learn/grades/${data.grade}/kanji?kanji=${nextUnit.kanji}&id=${id + 1}`;
         }
     }
     nextBtn.addEventListener("click", async function () {
         await Promise.all([
             playAudio("/sounds/next.mp3"),
-            addXP(data)
         ]);
-        window.location.href = link;
+        window.location.href = nextLink;
     });
 }
 
 async function addXP(data) {
     const learningStats = await fetchLearningStats();
-
     const isNewKanji =
         learningStats.currentGrade === data.grade &&
         learningStats.gradeProgress === id + Math.trunc(id / 10);
@@ -70,6 +69,7 @@ async function addXP(data) {
 fetchKanjiData(kanji).then(async (data) => {
         nextUnit = data.nextUnit;
         previousUnit = data.previousUnit;
+    addXP(data.kanjiData);
         displayKanjiData(data.kanjiData);
         displayButtons(data.kanjiData);
     }
