7a9b8f78dacfea7af71ba57eb20a47b2b8cb3c90
mayssaGH <mayssagnm@gmail.com>
2026-03-04 14:55:18 +0100

    refactor: move html templates to /pages folder

--- src/main/java/com/may55a/kotoba/controllers/IndexController.java
+++ src/main/java/com/may55a/kotoba/controllers/IndexController.java
@@ -10,60 +10,60 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class IndexController {
     @GetMapping("/")
     public String home() {
-        return "Home";
+        return "pages/Home";
     }
 
     @GetMapping("/signup")
     public String signup() {
-        return "Signup";
+        return "pages/Signup";
     }
 
     @GetMapping("/login")
     public String login() {
-        return "Login";
+        return "pages/Login";
     }
 
     @GetMapping("/learn")
     public String learn(Model model) {
         model.addAttribute("currentPath", "/learn");
-        return "Learn";
+        return "pages/Learn";
     }
 
     @GetMapping("/learn/grades/{grade}")
     public String learnGrade(@PathVariable String grade, Model model) {
         model.addAttribute("grade", grade);
-        return "Grade";
+        return "pages/Grade";
     }
     @GetMapping("/learn/grades/{grade}/kanji")
     public String learnKanji(@RequestParam String kanji, @RequestParam int id, @PathVariable String grade, Model model) {
         model.addAttribute("kanji", kanji);
         model.addAttribute("id", id);
         model.addAttribute("grade", grade);
-        return "Kanji";
+        return "pages/Kanji";
     }
 
     @GetMapping("/learn/grades/{grade}/tests/{test}")
     public String test(@PathVariable String grade, @PathVariable String test, Model model) {
         model.addAttribute("grade", grade);
         model.addAttribute("test", test);
-        return "Test";
+        return "pages/Test";
     }
 
     @GetMapping("/practice")
     public String practice(Model model) {
         model.addAttribute("currentPath", "/practice");
-        return "Practice";
+        return "pages/Practice";
     }
 
     @GetMapping("/favourites")
     public String favourites(Model model) {
         model.addAttribute("currentPath", "/favourites");
-        return "Favourites";
+        return "pages/Favourites";
     }
 
     @GetMapping("/favourites/{kanji}")
     public String learnKanji(@PathVariable String kanji, Model model) {
         model.addAttribute("kanji", kanji);
-        return "Kanji";
+        return "pages/Kanji";
     }
 }
